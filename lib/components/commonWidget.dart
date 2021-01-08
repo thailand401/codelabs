02c780269d9a420f5256a9b6f6c8bd22a1bdcfdd
Thor <okinawa35@gmail.com>
2021-01-08 10:01:14 +0700

    update common widget

--- lib/components/commonWidget.dart
+++ lib/components/commonWidget.dart
@@ -108,6 +108,27 @@ class Common {
           )),
     );
   }
+
+  static Widget cmback(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(left: 0, top: 10, bottom: 10),
+              child: Icon(Icons.keyboard_arrow_left, color: Colors.black),
+            ),
+            Text('Back',
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500))
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class ExtraChild {
--- lib/welcome/signupPage.dart
+++ lib/welcome/signupPage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:codelabs/welcome/Widget/bezierContainer.dart';
 import 'package:codelabs/welcome/loginPage.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:codelabs/components/entryField.dart';
+import 'package:codelabs/components/commonWidget.dart';
 
 class SignUpPage extends StatefulWidget {
   SignUpPage({Key key, this.title}) : super(key: key);
@@ -13,51 +15,6 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-  Widget _backButton() {
-    return InkWell(
-      onTap: () {
-        Navigator.pop(context);
-      },
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Row(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(left: 0, top: 10, bottom: 10),
-              child: Icon(Icons.keyboard_arrow_left, color: Colors.black),
-            ),
-            Text('Back',
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500))
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _entryField(String title, {bool isPassword = false}) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          TextField(
-              obscureText: isPassword,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  fillColor: Color(0xfff3f3f4),
-                  filled: true))
-        ],
-      ),
-    );
-  }
-
   Widget _submitButton() {
     return Container(
       width: MediaQuery.of(context).size.width,
@@ -143,9 +100,9 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget _emailPasswordWidget() {
     return Column(
       children: <Widget>[
-        _entryField("Your Name"),
-        _entryField("Mobile Number"),
-        _entryField("Password", isPassword: true),
+        EntryField.create("Your Name"),
+        EntryField.create("Mobile Number"),
+        EntryField.create("Password", isPassword: true),
       ],
     );
   }
@@ -186,7 +143,7 @@ class _SignUpPageState extends State<SignUpPage> {
                 ),
               ),
             ),
-            Positioned(top: 40, left: 0, child: _backButton()),
+            Positioned(top: 40, left: 0, child: Common.cmback(context)),
           ],
         ),
       ),
